f98d8e54f14239a6fd2ead4abdb1ed30c2a7c423
Jason Burich <jburich@gmail.com>
2024-06-30 09:12:33 -0500

    Initial Production Release

---
+++ medusajs-backend/src/api/admin/custom/sync/route.ts
@@ -0,0 +1,38 @@
+import { BatchJob, BatchJobService, MedusaRequest, MedusaResponse } from '@medusajs/medusa';
+
+export async function GET(req: MedusaRequest, res: MedusaResponse): Promise<void> {
+  res.json({ status: 'ok' });
+  res.sendStatus(200);
+}
+
+export async function POST(req: MedusaRequest, res: MedusaResponse): Promise<void> {
+  const batchJobService: BatchJobService = req.scope.resolve('batchJobService');
+  const responses: BatchJob[] = [];
+
+  // @ts-ignore
+  const body: { category: string; only: string[] } = req.body || { category: 'Error: Category is Required', only: [] };
+
+  if (!body.only || body.only.includes('sportlots')) {
+    responses.push(
+      await batchJobService.create({
+        type: 'sportlots-sync',
+        context: { category_id: body.category },
+        dry_run: false,
+        created_by: req.user.id,
+      }),
+    );
+  }
+
+  if (!body.only || body.only.includes('bsc')) {
+    responses.push(
+      await batchJobService.create({
+        type: 'bsc-sync',
+        context: { category_id: body.category },
+        dry_run: false,
+        created_by: req.user.id,
+      }),
+    );
+  }
+
+  res.json({ status: 'ok', category: body.category, job: responses });
+}
