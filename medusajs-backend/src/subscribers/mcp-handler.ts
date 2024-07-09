8375e54240f08fabd42731d244cfc55c60365928
Jason Burich <jburich@gmail.com>
2024-07-09 13:22:19 -0500

    Fixing MCP Image Uploads

--- medusajs-backend/src/subscribers/mcp-handler.ts
+++ medusajs-backend/src/subscribers/mcp-handler.ts
@@ -6,10 +6,11 @@ import {
   type SubscriberArgs,
   type SubscriberConfig,
 } from '@medusajs/medusa';
-import { remote } from 'webdriverio';
+import { remote, RemoteOptions } from 'webdriverio';
 import axios from 'axios';
 import * as fs from 'node:fs';
 import * as process from 'node:process';
+import JSZip from 'jszip';
 
 export default async function mcpHandler({
   data,
@@ -19,12 +20,12 @@ export default async function mcpHandler({
 }: SubscriberArgs<Record<string, any>>) {
   let mcp: WebdriverIO.Browser;
   let logger: Logger;
+  let activityId: string;
   try {
     logger = container.resolve<Logger>('logger');
     const { variantId, price, quantity } = data;
-    const activityId = logger.activity(`MCP Listing Update for ${variantId}: ${quantity}`);
-    const logProgress = (text: string) =>
-      logger.progress(activityId, `MCP Listing Update for ${product.title} [${productVariant.sku}] :: ${text}`);
+    // @ts-expect-error Logger should return the activity ID according to documentation but types do not indicate that it does
+    activityId = logger.activity(`MCP Listing Update for ${variantId}: ${quantity}`);
 
     const productVariantService: ProductVariantService = container.resolve('productVariantService');
     const productService: ProductService = container.resolve('productService');
@@ -33,13 +34,16 @@ export default async function mcpHandler({
     const product: Product = await productService.retrieve(productVariant.product_id, {
       relations: ['categories', 'images'],
     });
+    const logProgress = (text: string) =>
+      logger.progress(activityId, `MCP Listing Update for ${product.title} [${productVariant.sku}] :: ${text}`);
+
     const category = product.categories[0];
 
     logProgress(`Logging into MCP...`);
     mcp = await login();
 
     logProgress('Getting Site Counts...');
-    await browser_.url('https://mycardpost.com/edvedafi?tab=shop');
+    await mcp.url('edvedafi?tab=shop');
     let siteCount: number;
     const countFiled = await mcp.$('h2.card-count');
     const showingAll = await countFiled.getText();
@@ -76,9 +80,9 @@ export default async function mcpHandler({
         logProgress('Adding Card to MCP...');
         await mcp.$('button=Add Card').click();
         const form = await mcp.$('//form[@action="https://mycardpost.com/add-card"]');
-        const frontImage = await tempImage(product.images[0].url);
+        const frontImage = await tempImage(product.images[0].url, mcp);
         await form.$('#front_image').setValue(frontImage);
-        const backImage = await tempImage(product.images[1].url);
+        const backImage = await tempImage(product.images[1].url, mcp);
         await form.$('#back_image').setValue(backImage);
         await form.$('textarea[name="name"]').setValue(`${product.title} [${productVariant.sku}]`);
         await form.$('input[name="price"]').setValue(price === 99 ? 1 : price / 100);
@@ -147,8 +151,6 @@ export default async function mcpHandler({
 
         const toast = await mcp.$('.toast-message');
         await toast.waitForExist({ timeout: 30000 });
-        fs.rmSync(frontImage);
-        fs.rmSync(backImage);
         const resultText = await toast.getText();
         let message: string;
         if (resultText.indexOf('Successful') > -1) {
@@ -165,136 +167,147 @@ export default async function mcpHandler({
       }
     }
   } catch (error) {
-    logger?.error('mcpHandler::error: ', error);
+    logger?.failure(activityId, 'Failed to update MCP listing');
+    logger?.error('mcp::ERROR: ', error);
+    if (mcp) {
+      try {
+        await mcp.saveScreenshot(`/tmp/${activityId}.png`);
+        logger?.info(`mcp::Screenshot saved to /tmp/${activityId}.png`);
+        // await mcp.execute(() => {
+        //   window.scrollTo(0, 0);
+        // });
+        await mcp.scroll(-1000, -2000);
+        await mcp.saveScreenshot(`/tmp/${activityId}.top.png`);
+        logger?.info(`mcp::Screenshot saved to /tmp/${activityId}.top.png`);
+        logger?.info(`mcp::CurrentURL ${await mcp.getUrl()}`);
+      } catch (e) {
+        logger?.error('mcp::cleanup error:: Failed while attempting to log errors', e);
+      }
+    }
     throw error;
   } finally {
-    // if (mcp) {
-    //   await mcp.shutdown();
-    // }
+    if (mcp) {
+      // await mcp.deleteSession();
+    }
   }
 }
 
-let browser_: WebdriverIO.Browser;
 export const login = async () => {
-  const loginToSite = async () => {
-    if (!browser_) {
-      try {
-        console.log(JSON.stringify({
-          hostname: process.env.BROWSER_DOMAIN_PRIVATE,
-          port: parseInt(process.env.BROWSER_PORT_PRIVATE),
-          key: process.env.BROWSER_TOKEN,
-          capabilities: {
-            browserName: 'chrome',
-            'goog:chromeOptions': {
-              args: [
-                '--window-size=1920,1080',
-                '--disable-background-timer-throttling',
-                '--disable-backgrounding-occluded-windows',
-                '--disable-breakpad',
-                '--disable-component-extensions-with-background-pages',
-                '--disable-dev-shm-usage',
-                '--disable-extensions',
-                '--disable-features=TranslateUI,BlinkGenPropertyTrees',
-                '--disable-ipc-flooding-protection',
-                '--disable-renderer-backgrounding',
-                '--enable-features=NetworkService,NetworkServiceInProcess',
-                '--force-color-profile=srgb',
-                '--hide-scrollbars',
-                '--metrics-recording-only',
-                '--mute-audio',
-                '--headless',
-                '--no-sandbox'
-              ],
-            },
-          }
-        },null, 2));
-        browser_ = await remote({
-          hostname: process.env.BROWSER_DOMAIN_PRIVATE,
-          port: parseInt(process.env.BROWSER_PORT_PRIVATE),
-          key: process.env.BROWSER_TOKEN,
-          capabilities: {
-            browserName: 'chrome',
-            'goog:chromeOptions': {
-              args: [
-                '--window-size=1920,1080',
-                '--disable-background-timer-throttling',
-                '--disable-backgrounding-occluded-windows',
-                '--disable-breakpad',
-                '--disable-component-extensions-with-background-pages',
-                '--disable-dev-shm-usage',
-                '--disable-extensions',
-                '--disable-features=TranslateUI,BlinkGenPropertyTrees',
-                '--disable-ipc-flooding-protection',
-                '--disable-renderer-backgrounding',
-                '--enable-features=NetworkService,NetworkServiceInProcess',
-                '--force-color-profile=srgb',
-                '--hide-scrollbars',
-                '--metrics-recording-only',
-                '--mute-audio',
-                '--headless',
-                '--no-sandbox'
-              ],
-            },
-          },
-          baseUrl: 'https://mycardpost.com/',
-          // logLevel: 'error',
-        });
-      } catch (e) {
-        console.error('Failed to start browser', e);
-        throw e;
-      }
-    }
-    await browser_.url('login');
-    await browser_.$('input[type="email"]').setValue(process.env.MCP_EMAIL);
-    await browser_.$('input[type="password"]').setValue(process.env.MCP_PASSWORD);
-    await browser_.$('button=Login').click();
+  const config: RemoteOptions = {
+    capabilities: {
+      // @ts-ignore
+      browserName: 'chrome',
+      'goog:chromeOptions': {
+        // @ts-ignore
+        args: [
+          '--window-size=3000,2000',
+          '--disable-background-timer-throttling',
+          '--disable-backgrounding-occluded-windows',
+          '--disable-breakpad',
+          '--disable-component-extensions-with-background-pages',
+          '--disable-dev-shm-usage',
+          '--disable-extensions',
+          '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+          '--disable-ipc-flooding-protection',
+          '--disable-renderer-backgrounding',
+          '--enable-features=NetworkService,NetworkServiceInProcess',
+          '--force-color-profile=srgb',
+          '--hide-scrollbars',
+          '--metrics-recording-only',
+          '--mute-audio',
+          '--headless',
+          '--no-sandbox',
+        ],
+      },
+    },
+    baseUrl: 'https://www.mycardpost.com/',
+    // logLevel: 'error',
   };
-  if (!browser_) {
-    await loginToSite();
-  } else {
-    try {
-      try {
-        const url = await browser_.getUrl();
-        console.log('ur', url);
-        if (url.indexOf('mycardpost.com') === -1) {
-          await browser_.reloadSession();
-          await loginToSite();
-        }
-      } catch (e) {
-        await browser_.reloadSession();
-        await loginToSite();
-      }
-    } catch (e) {
-      try {
-        await browser_.shutdown();
-      } catch (e) {
-        //ignore
-      }
-      browser_ = null;
-      await loginToSite();
+  if (process.env.MCP_LOG_LEVEL) {
+    // @ts-ignore
+    config.logLevel = process.env.MCP_LOG_LEVEL;
+  }
+  if (process.env.BROWSER_DOMAIN_PRIVATE) {
+    config.path = '/webdriver';
+    config.hostname = process.env.BROWSER_DOMAIN_PRIVATE;
+    config.key = process.env.BROWSER_TOKEN;
+    config.capabilities['browserless:token'] = process.env.BROWSER_TOKEN;
+    if (process.env.BROWSER_PORT_PRIVATE === '443') {
+      config.protocol = 'https';
+      config.port = 443;
+    } else {
+      config.port = parseInt(process.env.BROWSER_PORT_PRIVATE);
     }
   }
+
+  const browser_ = await remote(config);
+  await browser_.url('login');
+  await browser_.$('input[type="email"]').setValue(process.env.MCP_EMAIL);
+  await browser_.$('input[type="password"]').setValue(process.env.MCP_PASSWORD);
+  await browser_.$('button=Login').click();
+
+  let toast: WebdriverIO.Element;
+  try {
+    toast = await browser_.$('.toast-message');
+  } catch (e) {
+    // no toast
+  }
+  // const isToastNotDisplayed = await toast.waitForDisplayed({
+  //   timeout: 1000, // Adjust the timeout as needed
+  //   reverse: true, // Wait for the element to be not displayed
+  //   timeoutMsg: 'Toast message is displayed', // Message to display if the toast appears
+  // });
+  if (toast && (await toast.isDisplayed())) {
+    const resultText = await toast.getText();
+    if (resultText.indexOf('Invalid Credentials') > -1) {
+      throw new Error('Invalid Credentials');
+    }
+  }
+
   return browser_;
 };
 
-async function tempImage(image: string): Promise<string> {
-  const file = `/tmp/${image}`;
-  if (fs.existsSync(file)) {
-    fs.rmSync(file);
-  }
+async function tempImage(imageName: string, browser: WebdriverIO.Browser): Promise<string> {
   const response = await axios.get(
-    `https://firebasestorage.googleapis.com/v0/b/hofdb-2038e.appspot.com/o/${image}?alt=media`,
-    {
-      responseType: 'stream',
-    },
+    `https://firebasestorage.googleapis.com/v0/b/hofdb-2038e.appspot.com/o/${imageName}?alt=media`,
+    { responseType: 'arraybuffer' },
   );
-  return new Promise((resolve, reject) => {
-    response.data
-      .pipe(fs.createWriteStream(file))
-      .on('error', reject)
-      .once('close', () => resolve(file));
-  });
+  const imageBuffer = Buffer.from(response.data, 'binary');
+
+  const zip = new JSZip();
+  zip.file(imageName, imageBuffer);
+  const zipBuffer = await zip.generateAsync({ type: 'nodebuffer' });
+  return await browser.file(zipBuffer.toString('base64'));
+  // await axios.post(uploadUrl, zipBuffer, {
+  //   headers: {
+  //     'Content-Type': 'application/zip',
+  //     'Content-Length': zipBuffer.length
+  //   }
+  // });
 }
+
+// async function tempImage(image: string, browser): Promise<string> {
+//   const file = `/tmp/${image}`;
+//   if (fs.existsSync(file)) {
+//     fs.rmSync(file);
+//   }
+//   const response = await axios.get(
+//     `https://firebasestorage.googleapis.com/v0/b/hofdb-2038e.appspot.com/o/${image}?alt=media`,
+//     {
+//       responseType: 'arraybuffer',
+//     },
+//   );
+//   const base64 = Buffer.from(response.data, 'binary').toString('base64');
+//
+//   return await browser.file(base64);
+//   //
+//   // return new Promise((resolve, reject) => {
+//   //   response.data
+//   //     .pipe(fs.createWriteStream(file))
+//   //     .on('error', reject)
+//   //     .once('close', () => resolve(file));
+//   // });
+// }
 
 export const config: SubscriberConfig = {
   event: 'mcp-listing-update',
