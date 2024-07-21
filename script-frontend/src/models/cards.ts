df8dee605758f56aa0708e0f3e62bd7b2c99dd09
Jason Burich <jburich@gmail.com>
2024-07-20 20:44:49 -0500

    Better price checking

--- script-frontend/src/models/cards.ts
+++ script-frontend/src/models/cards.ts
@@ -1,6 +1,5 @@
-import { type Category } from './setInfo';
+// import { type Category } from './setInfo';
 import type { Card } from './bsc';
-import { Product as MedusaProduct } from '@medusajs/client-types';
 
 export type SLCard = {
   cardNumber: string;
@@ -16,54 +15,54 @@ export type ProductImage = {
   file: string;
   url: string;
 };
+//
+// export type Product = {
+//   id: string;
+//   handle: string;
+//   type: string;
+//   categories: Category[];
+//   weight: number;
+//   length: number;
+//   width: number;
+//   height: number;
+//   depth?: number;
+//   origin_country: string;
+//   material: string;
+//   title: string;
+//   description: string;
+//   size: string;
+//   thickness?: string;
+//   lbs?: number;
+//   oz?: number;
+//   images: ProductImage[];
+//   variants: ProductVariant[];
+//   metadata: {
+//     cardNumber: string;
+//     player: string[];
+//     teams: string;
+//     sku: string;
+//     size: string;
+//     thickness: string;
+//     bsc?: string;
+//     sportlots?: string;
+//     cardName?: string;
+//     printRun?: string;
+//     autograph?: string;
+//     autographed?: boolean;
+//     features?: string;
+//   };
+// } & MedusaProduct;
 
-export type Product = {
-  id: string;
-  handle: string;
-  type: string;
-  categories: Category[];
-  weight: number;
-  length: number;
-  width: number;
-  height: number;
-  depth?: number;
-  origin_country: string;
-  material: string;
-  title: string;
-  description: string;
-  size: string;
-  thickness?: string;
-  lbs?: number;
-  oz?: number;
-  images: ProductImage[];
-  variants: ProductVariant[];
-  metadata: {
-    cardNumber: string;
-    player: string[];
-    teams: string;
-    sku: string;
-    size: string;
-    thickness: string;
-    bsc?: string;
-    sportlots?: string;
-    cardName?: string;
-    printRun?: string;
-    autograph?: string;
-    autographed?: boolean;
-    features?: string;
-  };
-} & MedusaProduct;
-
-export type ProductVariant = {
-  id: string;
-  title: string;
-  sku: string;
-  product?: Product;
-  prices: {
-    currency_code: string;
-    amount: number;
-  }[];
-};
+// export type ProductVariant = {
+//   id: string;
+//   title: string;
+//   sku: string;
+//   product?: Product;
+//   prices: {
+//     currency_code: string;
+//     amount: number;
+//   }[];
+// };
 
 export type Inventory = {
   id: string;
