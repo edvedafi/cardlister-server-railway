39b6b060455c4370ca56450fd662ba73721cd111
Jason Burich <jburich@gmail.com>
2024-08-11 08:44:23 -0500

    Better handling of pricing

--- medusajs-backend/src/strategies/sportlotsSalesStrategy.ts
+++ medusajs-backend/src/strategies/sportlotsSalesStrategy.ts
@@ -17,48 +17,53 @@ abstract class SportlotsSalesStrategy extends SaleStrategy<WebdriverIO.Browser> 
   }
 
   async getOrders(browser: WebdriverIO.Browser): Promise<SystemOrder[]> {
-    await browser.url('inven/dealbin/dealacct.tpl?ordertype=1a');
-
     const orders: SystemOrder[] = [];
-    const orderTable = await browser.$$('form[action="/inven/dealbin/dealupd.tpl"]');
-    for (const table of orderTable) {
-      //its divs all the way down!
-      let i = 0;
-      const divs = await table.$$(`div`);
-      const link = await divs[i].$('a');
-      const order: SystemOrder = {
-        id: await link.getText(),
-        customer: {
-          name: await link.getAttribute('title'),
-          username: await link.getText(),
-          email: (await link.getText()) + '@sportlots.com',
-        },
-        packingSlip: (await link.getAttribute('href')).replace("javascript:showFAQ('", '').replace("',1400,500)", ''),
-        lineItems: [],
-      };
-      i = 15; // skip a bunch of junk
-      while (i + 6 < divs.length) {
-        i++; //first is a blank div
-        const quantity = await divs[i++]?.getText();
-        const title = await divs[i++]?.getText();
-        const bin = await divs[i++]?.getText();
-        i++; // condition
-        const price = await divs[i++]?.getText();
-        const cardNumber = title
-          .split(' ')
-          .find((word) => word.startsWith('#'))
-          .replace('#', '');
-        order.lineItems.push({
-          quantity: parseInt(quantity.replace('\n0', '').trim()),
-          title: title,
-          sku: bin.indexOf('|') > 0 ? bin : `${bin}|${cardNumber}`,
-          cardNumber: cardNumber,
-          unit_price: parseInt(price.replace('.', '').replace('$', '').trim()),
-        });
+
+    const process = async (orderType: string) => {
+      await browser.url(`inven/dealbin/dealacct.tpl?ordertype=${orderType}`);
+      const orderTable = await browser.$$('form[action="/inven/dealbin/dealupd.tpl"]');
+      for (const table of orderTable) {
+        //its divs all the way down!
+        let i = 0;
+        const divs = await table.$$(`div`);
+        const link = await divs[i].$('a');
+        const order: SystemOrder = {
+          id: await link.getText(),
+          customer: {
+            name: await link.getAttribute('title'),
+            username: await link.getText(),
+            email: (await link.getText()) + '@sportlots.com',
+          },
+          packingSlip: (await link.getAttribute('href')).replace("javascript:showFAQ('", '').replace("',1400,500)", ''),
+          lineItems: [],
+        };
+        i = 15; // skip a bunch of junk
+        while (i + 6 < divs.length) {
+          i++; //first is a blank div
+          const quantity = await divs[i++]?.getText();
+          const title = await divs[i++]?.getText();
+          const bin = await divs[i++]?.getText();
+          i++; // condition
+          const price = await divs[i++]?.getText();
+          const cardNumber = title
+            .split(' ')
+            .find((word) => word.startsWith('#'))
+            .replace('#', '');
+          order.lineItems.push({
+            quantity: parseInt(quantity.replace('\n0', '').trim()),
+            title: title,
+            sku: bin.indexOf('|') > 0 ? bin : `${bin}|${cardNumber}`,
+            cardNumber: cardNumber,
+            unit_price: parseInt(price.replace('.', '').replace('$', '').trim()),
+          });
+        }
+        this.log(`Found order: ${JSON.stringify(order, null, 2)}`);
+        orders.push(order);
       }
-      this.log(`Found order: ${JSON.stringify(order, null, 2)}`);
-      orders.push(order);
-    }
+    };
+
+    await process('1a');
+    await process('1b');
 
     return orders;
   }
