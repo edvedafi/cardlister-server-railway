44b07f90932bb06160cda7e3e4681ececab27210
Jason Burich <jburich@gmail.com>
2024-09-19 05:42:17 -0500

    Improve MCP posting and puppeteer

--- medusajs-backend/src/utils/mcp.ts
+++ medusajs-backend/src/utils/mcp.ts
@@ -1,15 +1,24 @@
 import { PuppeteerHelper } from './puppeteer-helper';
 
 export async function login(pup: PuppeteerHelper): Promise<PuppeteerHelper> {
-  await pup.goto(`login`);
+  await pup.goto(`login`, 'profile');
 
-  await pup.locator('input[type="email"]').fill(process.env.MCP_EMAIL);
-  await pup.locator('input[type="password"]').fill(process.env.MCP_PASSWORD);
-  await pup.screenshot('login');
-  await (await pup.el({ locator: '.yellow-btn', text: 'Login' })).click();
+  if (pup.page.url().indexOf('login') > -1) {
+    await pup.page.evaluate(
+      (email, pass) => {
+        (<HTMLInputElement>document.querySelector('input[type="email"]')).value = email;
+        (<HTMLInputElement>document.querySelector('input[type="password"]')).value = pass;
+        (<HTMLInputElement>(
+          Array.from(document.querySelectorAll(`.yellow-btn`)).find((btn) => btn.textContent.trim() === 'Login')
+        )).click();
+      },
+      process.env.MCP_EMAIL,
+      process.env.MCP_PASSWORD,
+    );
 
-  await pup.locatorNotFound('.toast-message', 'Invalid Credentials: ');
-
+    await pup.locatorNotFound('.toast-message', 'Invalid Credentials: ');
+    await pup.waitForURL('edvedafi');
+  }
   await pup.goto('edvedafi?tab=shop');
 
   return pup;
