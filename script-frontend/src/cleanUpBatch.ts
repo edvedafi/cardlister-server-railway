2b3433943e100db5371c002318f23f00ce2baa06
Jason Burich <jburich@gmail.com>
2024-11-10 07:50:32 -0600

    Add parameters to cleanUpBatch.ts
    -w to watch the count of jobs
    -p to print all job info

--- script-frontend/src/cleanUpBatch.ts
+++ script-frontend/src/cleanUpBatch.ts
@@ -3,15 +3,41 @@ import 'zx/globals';
 import { useSpinners } from './utils/spinners';
 import { cancelSync, getAllBatchJobs } from './utils/medusa';
 import { ask } from './utils/ask';
+import { parseArgs } from './utils/parseArgs';
+import { sleep } from 'zx';
 
 $.verbose = false;
 
 dotenv.config();
+const args = parseArgs(
+  {
+    boolean: ['w', 'p'],
+    alias: {
+      w: 'watch',
+      p: 'print',
+    },
+  },
+  {
+    w: 'Watch the Count',
+    p: 'Print all open jobs',
+  },
+);
 
 const { log } = useSpinners('Sync', chalk.cyanBright);
 
 try {
   const jobs = await getAllBatchJobs();
+  if (args.print) {
+    jobs.forEach((job) => {
+      log(`Job ${job.id} - ${job.created_at}: ${job.type} => ${JSON.stringify(job.context)} ${job.status}`);
+    });
+  }
+  const start = jobs.length;
+  while (args.watch) {
+    const current = await getAllBatchJobs(!args.watch);
+    console.log(` ${current.length} / ${start}`);
+    await sleep(5000);
+  }
   if (jobs.length > 0) {
     const shouldCancel = await ask(`Cancel all ${jobs.length} jobs?`, false);
     if (shouldCancel) {
